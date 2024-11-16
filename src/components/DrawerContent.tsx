cd3cb4d1f8c827411a7529ab3a6b5f7df5a44589
keithwalsh <keithwalsh@gmail.com>
2024-11-16 18:58:49 +0000

    fix(navigation): resolve drawer content navigation behavior

--- src/components/DrawerContent.tsx
+++ src/components/DrawerContent.tsx
@@ -1,13 +1,40 @@
-import React, { useState } from "react"
-import { Box, List, Divider, ListItemIcon, Collapse } from "@mui/material"
-import { StyledListItem, StyledListItemText, NavigationListItemButton } from "./StyledComponents"
-import { NAVIGATION_ITEMS } from "./NavigationItems"
-import { Link as RouterLink, useLocation } from "react-router-dom"
-import { ExpandMore, ExpandLess } from "@mui/icons-material"
+import React, { useState } from "react";
+import { Box, List, Divider, ListItemIcon, Collapse } from "@mui/material";
+import { StyledListItem, StyledListItemText, NavigationListItemButton } from "./StyledComponents";
+import { NAVIGATION_ITEMS } from "./NavigationItems";
+import { Link as RouterLink, useLocation } from "react-router-dom";
+import { ExpandMore, ExpandLess } from "@mui/icons-material";
 
 interface DrawerContentProps {
-    isDesktop: boolean
-    setOpen: (open: boolean) => void
+    isDesktop: boolean;
+    setOpen: (open: boolean) => void;
+}
+
+type NavigationButtonProps = {
+    isSubItem?: boolean;
+    path?: string;
+    selected: boolean;
+    onClick: () => void;
+    children: React.ReactNode;
+};
+
+function NavigationButton({ isSubItem, path, selected, onClick, children }: NavigationButtonProps) {
+    const ButtonComponent = NavigationListItemButton as any;
+
+    return (
+        <ButtonComponent
+            component={path ? RouterLink : "div"}
+            {...(path && { to: path })}
+            selected={selected}
+            onClick={onClick}
+            sx={{
+                pl: isSubItem ? 4 : 1.4,
+                height: 48,
+            }}
+        >
+            {children}
+        </ButtonComponent>
+    );
 }
 
 export function DrawerContent({ isDesktop, setOpen }: DrawerContentProps) {
@@ -26,59 +53,44 @@ export function DrawerContent({ isDesktop, setOpen }: DrawerContentProps) {
         <Box sx={{ p: 2 }}>
             <List sx={{ padding: 0 }}>
                 {NAVIGATION_ITEMS.map(({ path, label, icon, showDividerBelow, subItems }) => {
-                    const isSelected = !subItems && (path === "/" 
-                        ? location.pathname === "/" 
-                        : location.pathname === path);
-                    
+                    const isSelected = !subItems && (path === "/" ? location.pathname === "/" : location.pathname === path);
                     const isExpanded = expandedItem === path;
 
                     return (
                         <React.Fragment key={path}>
                             <StyledListItem disablePadding>
-                                <NavigationListItemButton
-                                    component={subItems ? 'div' : RouterLink}
-                                    to={subItems ? '#' : path}
+                                <NavigationButton
+                                    isSubItem={false}
+                                    path={subItems ? undefined : path}
                                     selected={isSelected}
                                     onClick={() => handleItemClick(path, !!subItems)}
-                                    sx={{
-                                        px: 1.4,
-                                        height: 48,
-                                    }}
                                 >
                                     <ListItemIcon sx={{ minWidth: 36 }}>{icon}</ListItemIcon>
                                     <StyledListItemText primary={label} />
-                                    {subItems && (
-                                        isExpanded ? <ExpandLess /> : <ExpandMore />
-                                    )}
-                                </NavigationListItemButton>
+                                    {subItems && (isExpanded ? <ExpandLess /> : <ExpandMore />)}
+                                </NavigationButton>
                             </StyledListItem>
-                            
+
                             {subItems && (
                                 <Collapse in={isExpanded} timeout="auto" unmountOnExit>
                                     <List component="div" disablePadding>
                                         {subItems.map((subItem) => (
                                             <StyledListItem key={subItem.path} disablePadding>
-                                                <NavigationListItemButton
-                                                    component={RouterLink}
-                                                    to={subItem.path}
+                                                <NavigationButton
+                                                    isSubItem={true}
+                                                    path={subItem.path}
                                                     selected={location.pathname === subItem.path}
                                                     onClick={() => !isDesktop && setOpen(false)}
-                                                    sx={{
-                                                        pl: 4,
-                                                        height: 40,
-                                                    }}
                                                 >
-                                                    <ListItemIcon sx={{ minWidth: 36 }}>
-                                                        {subItem.icon}
-                                                    </ListItemIcon>
+                                                    <ListItemIcon sx={{ minWidth: 36 }}>{subItem.icon}</ListItemIcon>
                                                     <StyledListItemText primary={subItem.label} />
-                                                </NavigationListItemButton>
+                                                </NavigationButton>
                                             </StyledListItem>
                                         ))}
                                     </List>
                                 </Collapse>
                             )}
-                            
+
                             {showDividerBelow && <Divider sx={{ my: 1 }} />}
                         </React.Fragment>
                     );
